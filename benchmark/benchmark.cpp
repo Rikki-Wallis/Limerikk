44bfc57de35a75e73a3d50f93d17e02e38725337
jeremyylimmm <jeremylim.0304@gmail.com>
2026-05-21 19:08:49 +1000

    Increase benchmark depth

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,7 +1,7 @@
 #include "limerikk.h"
 
 int main() {
-    for (int depth = 1; depth <= 5; ++depth) {
+    for (int depth = 1; depth <= 7; ++depth) {
         Position pos = *Position::parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
         std::atomic<bool> should_stop;
