608ceef86bf8344beff443131f90b5c80ffc9e25
jeremyylimmm <jeremylim.0304@gmail.com>
2026-05-31 18:01:01 +1000

    Update benchmark depth

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,7 +5,7 @@
 int main() {
     std::unique_ptr<SearchContext> ctx = std::make_unique<SearchContext>(&null_budgeter);
 
-    for (int depth = 1; depth <= 6; ++depth) {
+    for (int depth = 1; depth <= 10; ++depth) {
         Position pos = *Position::parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
         SearchStatistics stats;
